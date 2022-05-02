d111c8bc8b6eabd1941a08634123096e046633ee
Artyom Ionov <ionov@navalny-team.org>
2022-05-02 18:24:37 +0300

    CBA to Description.ext

--- missions/WarlordSS220.zargabad/configs/cba.hpp
+++
@@ -1,9 +0,0 @@
-// Функции CBA тут и далее
-//Функция вызова переснарядки магазинов - Shield
-class Extended_PreInit_EventHandlers
-{
-	class outlw_magRepack
-	{
-		Init = "outlw_magRepack_Var = execVM 'XEH_preInit.sqf'";
-	};
-};
